04e5517d96aa51ef6e749d6c63fe7333673ca2ea
Morten Møller Petersen <mortenanders@gmail.com>
2017-06-15 09:31:24 +0200

    Extended unfluff example with multiple urls for comparison

--- unfluff/index.js
+++ unfluff/index.js
@@ -1,7 +1,18 @@
-var url = "http://www.tipsbladet.dk/nyhed/rygter/torsdagens-rygteboers-verratti-og-agent-til-middag-med-messi";
+var urls = [];
+urls.push("http://www.tipsbladet.dk/nyhed/rygter/torsdagens-rygteboers-verratti-og-agent-til-middag-med-messi");
+urls.push("http://ekstrabladet.dk/sport/fodbold/dansk_fodbold/superligaen/fc_koebenhavn/dansk-klub-var-klar-til-at-koebe-okore-fri/6707874");
+urls.push("http://www.bold.dk/fodbold/nyheder/avis-nicklas-p-i-dialog-med-mechelen/");
+
 var request = require('request');
 var unfluff = require('unfluff');
+var async = require('async');
 
-request(url, function(err, response, body){
-	console.log(JSON.stringify(unfluff(body), null, 2));
+function parse(url, callback){
+	request(url, function(err, response, body){
+		callback(err, unfluff(body));
+	})	
+}
+
+async.map(urls, parse, function(err, results){
+	console.log(JSON.stringify(results, null, 2));
 })
